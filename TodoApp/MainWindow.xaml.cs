7b250969634e27e5170320107cae6f4e0e86c58e
OliverMustonen <oliverbleach99@gmail.com>
2024-02-25 16:28:47 +0100

    Simple WPF(Windows Presentation Foundation) as a simple Todo with no remove or check.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TodoApp
+{
+    
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<string> items = [];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Input_Name.Focus();
+
+            List_TaskList.ItemsSource = items;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(Input_Name.Text)) {
+                items.Add(Input_Name.Text);
+                Input_Name.Text = string.Empty;
+            }
+        }
+    }
+}
